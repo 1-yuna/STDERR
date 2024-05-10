1ae73ce8101bffe6637903fb15ef4136616e4f6b
yuna <micha0215@tukorea.ac.kr>
2024-05-11 03:17:08 +0900

    [style] title, tag 컴포넌트 완료

--- Project-Stderr/frontend/src/pages/HomePage.jsx
+++ Project-Stderr/frontend/src/pages/HomePage.jsx
@@ -1,14 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import Background from "../components/Background";
 import Question from "../components/ContentElements/Question.jsx";
 import Code from "../components/ContentElements/Code.jsx";
-import Background from "../components/Background";
+import TitleTag from "../components/ContentElements/TitleTag.jsx";
 
 function HomePage() {
   return (
     <Background>
       <Question />
       <Code />
+      <TitleTag name="Title" />
+      <TitleTag name="Tags" />
     </Background>
   );
 }
